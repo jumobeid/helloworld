1eb6559b7e608ea154818ffc218e08c34f7e5dfc
jumana Obeid <jumana-o@live.com>
2017-02-06 17:49:53 +0400

    change random text qoute

--- index.php
+++ index.php
@@ -2,14 +2,36 @@
 <html>
 <head>
 <title>intro</title>
+<style>
+h1{
+  text-align: center;
+}
+img{
+  border-radius: 50px 50px 50px 50px;
+  text-align: center;
+}
+</style>
 </head>
 <body>
 
   <h1>Jumana Obeid<h1>
 
-  <img src="images/jumana.jpg" alt="jumana">
+  <img src="images/jumana.jpg" alt="jumana"><br>
 
-    <a href="http://www.w3schools.com/html/">Visit our HTML tutorial</a> 
+  <a href="http://www.w3schools.com/html/">Visit our HTML tutorial</a><br>
+  <?php
+
+   //this code is from http://stackoverflow.com/questions/5857033/displaying-random-text
+
+   $messages = array(
+       "love is stronger than justice",
+       "Men are like wine,some turn to vineger but others improve by age",
+       "Every thing you ever wanted is on the other side of fear"
+   );
+
+   echo $messages[rand(0, count($messages) - 1)];
+  ?>
+
 
 </body>
 </html>
