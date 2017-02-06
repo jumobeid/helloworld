59e2e339416b6c1c380030ff82621034c6fe3fff
jumana Obeid <jumana-o@live.com>
2017-02-06 16:11:36 +0400

    added a link

--- index.php
+++ index.php
@@ -1,3 +1,15 @@
-<h1>Jumana Obeid<h1>
+<!DOCTYPE html>
+<html>
+<head>
+<title>intro</title>
+</head>
+<body>
+
+  <h1>Jumana Obeid<h1>
 
   <img src="images/jumana.jpg" alt="jumana">
+
+    <a href="http://www.w3schools.com/html/">Visit our HTML tutorial</a> 
+
+</body>
+</html>
